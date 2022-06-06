c2de6841d2c3d8ce89e4cc28329fc2bd4aa8ffb9
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2022-06-06 19:31:31 -0400

    avance

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+
 from django.shortcuts import render
 from .models import Producto
 from .forms import ProductoForm
@@ -32,19 +32,22 @@ def crud(request):
     return render(request, 'core/Crud.html', datos) #claro esto es solo una prueba la pagina puede cambiar
     
 def formulario(request):
+    datos = {
+        'form': ProductoForm()
+    }
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = ProductoForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            formulario.save()
+            form.save()
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            datos['mensaje'] = "Datos Guardados Correctamente"
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = ProductoForm()
 
-    return render(request, 'core/formulario.html', {'form': form})
+    return render(request, 'core/formulario.html', datos)
 
