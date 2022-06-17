f44f1fe0af33fe8c062cfe6308a73046dab8bfe4
FerHenn <102255890+FerHenn@users.noreply.github.com>
2022-06-17 08:28:46 -0400

    commited

--- DjangoFundacion/settings.py
+++ DjangoFundacion/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'django.contrib.humanize',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK ='bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
